c7ba7679f3a75f10640ec58fd50df91c1ddd983c
chengxinwei <593040793@qq.com>
2015-11-11 11:17:02 +0800

    UBT 工程建立

---
+++ ubt-console-server/src/main/java/com/howbuy/cc/ubt/App.java
@@ -0,0 +1,13 @@
+package com.howbuy.cc.ubt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
